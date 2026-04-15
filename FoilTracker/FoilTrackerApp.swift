300a9e716c34389c918d5cbdc9c64d02132cb668
Dimitri Crombecq <dimitri.crombecq@outlook.be>
2026-04-15 21:10:10 +0200

    Initial Commit

---
+++ FoilTracker/FoilTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoilTrackerApp.swift
+//  FoilTracker
+//
+//  Created by Dimitri Crombecq on 15/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FoilTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
